47b3a9a05fcc1ab5f30e30548d3537d1422b5dde
Ben Chapman <ben@dismiz.com>
2013-03-28 16:02:38 +0000

    Create check_setup.php

---
+++ check_setup.php
@@ -0,0 +1,19 @@
+<h1>PHP testing</h1>
+
+<hr/>
+<h3>Environment variables</h3>
+<ul>
+  <li><b>$_SERVER['EY_TESTING']</b>: <?php echo isset($_SERVER['EY_TESTING'])?:"----FAIL----"; ?></li>
+  <li><b>$_SERVER['HTTPS']</b>: <?php echo isset($_SERVER['HTTPS'])?:"Not HTTPS? OR FAIL"; ?></li>
+</ul>
+
+<hr/>
+<h3>Rewrites</h3>
+<b>The following should be an image:</b><br/>
+<img src="/wp-content/themes/twentyten/images/headers/berries.jpg" width="200" /><br/><br/>
+
+<b>The following should be a Wordpress generated 404 in an iframe:</b><br/>
+<iframe src="/this-is-nothing/test.png" width="200" />
+
+<hr/>
+<h3></h3>
